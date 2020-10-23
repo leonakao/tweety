7ec28c58d781b711605bb0ba2fa0aab0864fa69e
Leonardo Nakao <leonakao.dev@gmail.com>
2020-10-23 18:45:17 -0300

    clear default layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex">
+        <div class="flex-1">1</div>
+        <div class="flex-1">2</div>
+        <div class="flex-1">3</div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,24 +19,19 @@
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.js" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-dropdown')
+        <div id="app">
+            <section class="px-8">
+                <header class="container mx-auto">
+                    <h1>Tweety</h1>
+                </header>
+            </section>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <section class="px-8">
+                <main class="container mx-auto">
+                    @yield('content')
+                </main>
+            </section>
         </div>
-
-        @stack('modals')
-
-        @livewireScripts
     </body>
 </html>
