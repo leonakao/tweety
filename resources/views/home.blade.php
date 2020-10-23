33851977f5f04814645d98c7082851da304f37e1
Leonardo Nakao <leonakao.dev@gmail.com>
2020-10-23 19:04:34 -0300

    create sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     <div class="flex">
-        <div class="flex-1">1</div>
+        <div class="flex-1">
+            @include('_sidebar-links')
+        </div>
         <div class="flex-1">2</div>
         <div class="flex-1">3</div>
     </div>
