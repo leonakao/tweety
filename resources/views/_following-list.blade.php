8171bdc567f9cd53f0dc9c0e176c4ab6afe7c6d3
Leonardo Nakao <leonakao.dev@gmail.com>
2020-10-26 21:00:10 -0300

    change term "friends" to "following"

--- resources/views/_following-list.blade.php
+++ resources/views/_following-list.blade.php
@@ -1,17 +1,17 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 
 <ul>
-    @foreach(auth()->users()->follows as $user)
+    @foreach(auth()->user()->follows as $user)
         <li class="mb-4">
             <div class="flex items-center text-sm">
                 <img
-                    src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
-                    alt=""
+                    src="{{ $user->avatar }}"
+                    alt="{{ $user->name }}"
                     class="rounded-full mr-2"
                     width="40"
                 >
 
-                John Doe
+                {{ $user->name }}
             </div>
         </li>
     @endforeach
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
-            @include('_friends-list')
+            @include('_following-list')
         </div>
     </div>
 @endsection
