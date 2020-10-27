7a1ea464b2f03d4c8ba5707e9108231bc3953332
Leonardo Nakao <leonakao.dev@gmail.com>
2020-10-26 22:14:26 -0300

    create a _timeline file

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -4,10 +4,6 @@
 
     @include('_publish-tweet-panel')
 
-    <div class="border border-gray-300 rounded-lg">
-        @foreach($tweets as $tweet)
-            @include('_tweet')
-        @endforeach
-    </div>
+    @include('_timeline')
 
 @endsection
