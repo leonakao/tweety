4ce6780d0d5ad382626470381a764f331520f03a
Leonardo Nakao <leonakao.dev@gmail.com>
2020-10-28 12:29:17 -0300

    add timestamp on follows relationship

--- app/Traits/Followable.php
+++ app/Traits/Followable.php
@@ -22,6 +22,6 @@ trait Followable {
     }
 
     public function follows() {
-        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id')->withTimestamps();
     }
 }
