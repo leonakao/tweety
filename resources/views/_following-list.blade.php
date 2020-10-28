4c9df9c3e9f5c658c332fb48522d57e6325ecc7a
Leonardo Nakao <leonakao.dev@gmail.com>
2020-10-28 17:54:43 -0300

    add message if no have following

--- resources/views/_following-list.blade.php
+++ resources/views/_following-list.blade.php
@@ -1,7 +1,7 @@
 <h3 class="font-bold text-xl mb-4">Following</h3>
 
 <ul>
-    @foreach(current_user()->follows as $user)
+    @forelse(current_user()->follows as $user)
         <li class="mb-4">
             <div class="text-sm">
                 <a
@@ -20,5 +20,9 @@
                 </a>
             </div>
         </li>
-    @endforeach
+    @empty
+        <li>
+            No following yet
+        </li>
+    @endforelse
 </ul>
