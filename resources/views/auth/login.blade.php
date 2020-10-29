aa5c06ceb98c8c7491574ee5740d2adb3d2f1b7c
Leonardo Nakao <leonakao.dev@gmail.com>
2020-10-29 11:08:39 -0300

    improve login and register page layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,53 @@
 <x-master>
     <div class="container mx-auto px-6 py-4">
-        <div class="row justify-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="font-bold text-xl mb-4 text-center">Register</h2>
-                    </div>
+        <div class="flex justify-center">
+            <div class="card bg-blue-200 px-12 py-8 rounded-lg min-w-1/3">
+                <div class="card-header">
+                    <h2 class="font-bold text-xl mb-4 text-center">Login</h2>
+                </div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-                            <div>
-                                <label for="email" value="Email"> Email </label>
-                                <input id="email" class="border-b border-black-500 block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-                                @error('email')
-                                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                                @enderror
-                            </div>
+                        <div>
+                            <label for="email" value="Email"> Email </label>
+                            <input id="email" class="border-b border-black-500 block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                            @error('email')
+                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
 
-                            <div class="mt-4">
-                                <label for="password" value="Password"> Password </label>
-                                <input id="password" class="border-b border-black-500 block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-                                @error('password')
-                                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                                @enderror
-                            </div>
+                        <div class="mt-4">
+                            <label for="password" value="Password"> Password </label>
+                            <input id="password" class="border-b border-black-500 block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                            @error('password')
+                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
 
-                            <div class="block mt-4">
-                                <label for="remember_me" class="flex items-center">
-                                    <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
-                                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                                </label>
-                                @error('remember')
-                                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                                @enderror
-                            </div>
+                        <div class="block mt-4">
+                            <label for="remember_me" class="flex items-center">
+                                <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
+                                <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                            </label>
+                            @error('remember')
+                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
 
-                            <div class="flex items-center justify-end mt-4">
-                                @if (Route::has('password.request'))
-                                    <a class="underline text-sm text-gray-600 hover:text-gray-900 mr-2" href="{{ route('password.request') }}">
-                                        Forgot your password?
-                                    </a>
-                                @endif
+                        <div class="flex items-center justify-end mt-6">
+                            @if (Route::has('password.request'))
+                                <a class="underline text-sm text-gray-600 hover:text-gray-900 mr-2" href="{{ route('password.request') }}">
+                                    Forgot your password?
+                                </a>
+                            @endif
 
-                                <button type="submit" class="px-5 py-3 rounded text-sm uppercase bg-blue-600 text-white">
-                                    Login
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                            <button type="submit" class="px-5 py-3 rounded text-sm uppercase bg-blue-600 text-white">
+                                Login
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,67 +1,65 @@
 <x-master>
     <div class="container mx-auto px-6 py-4">
-        <div class="row justify-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="font-bold text-xl mb-4 text-center">Register</h2>
-                    </div>
+        <div class="flex justify-center">
+            <div class="card bg-blue-200 px-12 py-8 rounded-lg min-w-1/3">
+                <div class="card-header">
+                    <h2 class="font-bold text-xl mb-4 text-center">Register</h2>
+                </div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}">
-                            @csrf
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
-                            <div>
-                                <label for="username" value="Username"> Username </label>
-                                <input id="username" class="border-b border-black-500 block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
-                                @error('username')
-                                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                                @enderror
-                            </div>
+                        <div>
+                            <label for="username" value="Username"> Username </label>
+                            <input id="username" class="border-b border-black-500 block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+                            @error('username')
+                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
 
-                            <div>
-                                <label for="name" value="Name"> Name </label>
-                                <input id="name" class="border-b border-black-500 block mt-1 w-full" type="text" name="name" :value="old('name')" required autocomplete="name" />
-                                @error('name')
-                                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                                @enderror
-                            </div>
+                        <div class="mt-4">
+                            <label for="name" value="Name"> Name </label>
+                            <input id="name" class="border-b border-black-500 block mt-1 w-full" type="text" name="name" :value="old('name')" required autocomplete="name" />
+                            @error('name')
+                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
 
-                            <div class="mt-4">
-                                <label for="email" value="Email"> Email </label>
-                                <input id="email" class="border-b border-black-500 block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-                                @error('email')
-                                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                                @enderror
-                            </div>
+                        <div class="mt-4">
+                            <label for="email" value="Email"> Email </label>
+                            <input id="email" class="border-b border-black-500 block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                            @error('email')
+                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
 
-                            <div class="mt-4">
-                                <label for="password" value="Password"> Password </label>
-                                <input id="password" class="border-b border-black-500 block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-                                @error('password')
-                                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                                @enderror
-                            </div>
+                        <div class="mt-4">
+                            <label for="password" value="Password"> Password </label>
+                            <input id="password" class="border-b border-black-500 block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                            @error('password')
+                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
 
-                            <div class="mt-4">
-                                <label for="password_confirmation" value="Confirm Password"> Confirm Password </label>
-                                <input id="password_confirmation" class="border-b border-black-500 block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-                                @error('password_confirmation')
-                                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                                @enderror
-                            </div>
+                        <div class="mt-4">
+                            <label for="password_confirmation" value="Confirm Password"> Confirm Password </label>
+                            <input id="password_confirmation" class="border-b border-black-500 block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                            @error('password_confirmation')
+                                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                            @enderror
+                        </div>
 
-                            <div class="flex items-center justify-end mt-4">
-                                <a class="underline text-sm text-gray-600 hover:text-gray-900 mr-2" href="{{ route('login') }}">
-                                    Already registered?
-                                </a>
+                        <div class="flex items-center justify-end mt-6">
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900 mr-2" href="{{ route('login') }}">
+                                Already registered?
+                            </a>
 
-                                <button type="submit" class="px-5 py-3 rounded text-sm uppercase bg-blue-600 text-white">
-                                    Register
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                            <button type="submit" class="px-5 py-3 rounded text-sm uppercase bg-blue-600 text-white">
+                                Register
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
