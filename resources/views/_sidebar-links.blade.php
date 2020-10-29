1e76978a343c04dfda2794a21023b491f0ea3125
Leonardo Nakao <leonakao.dev@gmail.com>
2020-10-29 15:31:28 -0300

    remove not implemented options in sidebart

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -10,26 +10,6 @@
         </a>
     </li>
     <li>
-        <a href="/Notifications" class="font-bold text-lg mb-4 block">
-            Notifications
-        </a>
-    </li>
-    <li>
-        <a href="/messages" class="font-bold text-lg mb-4 block">
-            Messages
-        </a>
-    </li>
-    <li>
-        <a href="/bookmarks" class="font-bold text-lg mb-4 block">
-            Bookmarks
-        </a>
-    </li>
-    <li>
-        <a href="/lists" class="font-bold text-lg mb-4 block">
-            Lists
-        </a>
-    </li>
-    <li>
         <a href="{{ current_user()->path() }}" class="font-bold text-lg mb-4 block">
             Profile
         </a>
